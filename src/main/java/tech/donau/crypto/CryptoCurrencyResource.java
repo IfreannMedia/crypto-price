6e413033b3c149c03cdb7b140fe14f39c02e5ef4
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-04-09 13:39:56 +0200

    multi form data request ahndling

--- src/main/java/tech/donau/crypto/CryptoCurrencyResource.java
+++ src/main/java/tech/donau/crypto/CryptoCurrencyResource.java
@@ -3,18 +3,17 @@ package tech.donau.crypto;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import tech.donau.crypto.data.Currency;
+import tech.donau.crypto.data.MultiPartBody;
 import tech.donau.crypto.service.CurrencyService;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayInputStream;
 import java.util.Collection;
 import java.util.concurrent.CompletionStage;
 
-@Path("/crypto")
+@Path("crypto")
 public class CryptoCurrencyResource {
 
     @Inject
@@ -25,5 +24,23 @@ public class CryptoCurrencyResource {
     @Produces(MediaType.APPLICATION_JSON)
     public CompletionStage<Collection<Currency>> getCrypto(@QueryParam("id") String id) {
         return currencyService.getCurrency(id);
+    }
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String echoFile(String body) {
+        return body;
+    }
+
+
+    @Path("/file")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String callEcho() {
+        final MultiPartBody multiPartBody = new MultiPartBody();
+        multiPartBody.setFile(new ByteArrayInputStream("HELLOW WRLD".getBytes()));
+        multiPartBody.setName("file.txt");
+        return currencyService.sendFile(multiPartBody);
     }
 }
